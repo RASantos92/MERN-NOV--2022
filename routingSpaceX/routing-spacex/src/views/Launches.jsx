be95e2b1621a3b58021f5240e6fa6c143751b98e
RASantos92 <rasbms81608@gmail.com>
2022-11-01 16:11:57 -0500

    11/1 pm spaceX finished

--- routingSpaceX/routing-spacex/src/views/Launches.jsx
+++ routingSpaceX/routing-spacex/src/views/Launches.jsx
@@ -1,40 +1,45 @@
-import { useEffect,useState } from "react"
-import axios from 'axios'
+import { useEffect, useState } from "react"
 import { LoadingSpinner } from "../components/LoadingSpinner";
-export const Launches = (props)=> {
+import { Link } from "react-router-dom";
+import { getAllLaunches } from "../services/spacexApiServices";
+
+export const Launches = (props) => {
     const [launches, setLaunches] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
         setIsLoading(true)
-        setTimeout(()=>{
-            axios
-            .get('https://api.spacexdata.com/v4/launches')
-            .then((res) => {
-                console.log(res.data);
-                setLaunches(res.data);
-            })
-            .catch((err) => {
-                console.log(err);
-            })
-            .finally(()=>{
-                setIsLoading(false)
-            })
+        setTimeout(() => {
+            getAllLaunches()
+                .then((data) => {
+                    const launchesWithImages = data.filter((launch) => {
+                        return launch.links.flickr.original.length > 0
+                    })
+                    console.log(data);
+                    setLaunches(launchesWithImages);
+                })
+                .catch((err) => {
+                    console.log(err);
+                })
+                .finally(() => {
+                    setIsLoading(false)
+                })
         }, 2000)
-        
-    },[])
+    }, [])
 
     return (
         <div className="flex-col align-items-center text-center">
             {
-                isLoading && <LoadingSpinner/>
+                isLoading && <LoadingSpinner />
             }
             {
                 launches.map((launch, i) => {
-                    const {name, date_local} = launch;
+                    const { name, date_local, id } = launch;
                     return (
-                        <div key={i}className="w-25pct mb-md shadow rounded">
-                            <h2>{name}</h2>
+                        <div key={i} className="w-25pct mb-md shadow rounded">
+                            <Link to={`/oneLaunch/${id}`}>
+                                <h2>{name}</h2>
+                            </Link>
                             <p>Date: {date_local}</p>
                         </div>
                     )
--- routingSpaceX/routing-spacex/src/views/OneLaunch.jsx
+++ routingSpaceX/routing-spacex/src/views/OneLaunch.jsx
@@ -1,7 +1,81 @@
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from 'react'
+import { LoadingSpinner } from "../components/LoadingSpinner";
+import { getOneLaunch } from "../services/spacexApiServices";
 
 export const OneLaunch = (props) => {
     const { id } = useParams();
-    return <h2>One Launch = id: {id}</h2>
+    const [isLoading, setIsLoading] = useState(false)
+    const [launch, setLaunch] = useState(null)
+
+    useEffect(() => {
+        setIsLoading(true)
+        setTimeout(() => {
+            getOneLaunch(id)
+                .then((data) => {
+                    console.log(data);
+                    setLaunch(data);
+                })
+                .catch((err) => {
+                    console.log(err);
+                })
+                .finally(() => {
+                    setIsLoading(false)
+                })
+        }, 2000)
+
+    }, [id])
+
+    if (launch === null) {
+        return <LoadingSpinner />
+    }
+
+    const {name,date_local,details,links} = launch;
+    const {flickr, article} = links
+    const {original: originalImageUrls} = flickr
+
+    // const {
+    //     date_local,
+    //     details,
+    //     name,
+    //     links: {
+    //         flickr: { original: originalImageUrls },
+    //     },
+    //     links: {
+    //         article
+    //     }
+    // } = launch;
+
+    return <div className="flex-col align-items-center text-center">
+        <h2>{name}</h2>
+        <h4>Date:</h4>
+        <p>{date_local}</p>
+        <h4>Details:</h4>
+        <p>{details}</p>
+        {
+            originalImageUrls.map((url) => {
+                return (
+                    <img
+                        className="shadow-img rounded mb-md"
+                        src={url}
+                        alt="Launch"
+                        width="60%" />
+                )
+            })
+        }
+        {
+
+            article && (
+                <iframe
+                    title="Launch Article"
+                    src={article}
+                    width="50%"
+                    height="800"
+                    allowfullscreen=""
+                    loading="lazy"
+                />
+            )
+        }
+    </div>
 }
 
